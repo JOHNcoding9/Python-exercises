4dd515b66e8ab71533adb92bfc6a8db4628ba240
João Vitor de Oliveira Lima <160860021+JOHNcoding9@users.noreply.github.com>
2024-07-18 21:12:25 -0300

    Update exercicio 51 guanabara.py

--- exercicio 51 guanabara.py
+++ exercicio 51 guanabara.py
@@ -14,3 +14,19 @@ for i in range(1,11):
  #armazenamento da progressão da PA na lista "dez_primeiros"
  dez_primeiros.append(termo)
 print(f"os dez primeiros numeros desta PA são: {dez_primeiros}")
+
+# OU
+
+print("=-="*30)
+print("                Os 10 primeiros termos de qualquer PA")
+print("=-="*30)
+#definidno o termo e a razao da PA
+termo=int(input("Insira o primeiro termo da PA: "))
+razao=int(input("Digite a razão desta PA: "))
+dez_primeiros=[]
+
+# definir a range da PA, sendo o numero inicial aquele digitado pelo usuário, enquanto que o numero final será a repetição do termo mais a razão 10 vezes.
+# o ultimo numero da range é a razão da Pa escolhida pelo usuario
+for i in range(termo,(termo+razao*10),razao):
+ dez_primeiros.append(i)
+print(f"os dez primeiros numeros desta PA são: {dez_primeiros}")
